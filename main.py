3abb4ef97011f4c98fd7faa4330f7a429a8eca20
Thomas Tunc <28352452+thomastunc@users.noreply.github.com>
2024-09-20 15:08:20 +0200

    Fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 @click.command(no_args_is_help=True)
 @click.argument('input', type=click.Path(exists=True))
-@click.argument('output', type=click.Path(exists=True))
+@click.argument('output', type=click.Path(exists=False))
 def csv_to_parquet(input, output):
     df = pd.read_csv(input, delimiter=';')
     df.to_parquet(output)
