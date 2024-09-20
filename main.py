d8d1e0a0b04f065a0c16e6ae0123cc96c41ca5d6
Thomas Tunc <28352452+thomastunc@users.noreply.github.com>
2024-09-20 15:07:47 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import click
+import pandas as pd
+
+
+@click.command(no_args_is_help=True)
+@click.argument('input', type=click.Path(exists=True))
+@click.argument('output', type=click.Path(exists=True))
+def csv_to_parquet(input, output):
+    df = pd.read_csv(input, delimiter=';')
+    df.to_parquet(output)
+
+
+if __name__ == '__main__':
+    csv_to_parquet()
